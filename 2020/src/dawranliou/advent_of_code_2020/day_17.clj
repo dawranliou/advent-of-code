64a5f3832b55fc7d3c35a21d088c694d5e5a72e2
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-12-17 16:42:27 +0800

    Day 17 part 2

--- 2020/src/dawranliou/advent_of_code_2020/day_17.clj
+++ 2020/src/dawranliou/advent_of_code_2020/day_17.clj
@@ -5,21 +5,21 @@
 ..#
 ###")
 
-(defn index [input]
+(defn index-3d [input]
   (->> (str/split-lines input)
        (mapcat (fn [y xs]
                  (keep-indexed (fn [x ch] (when (= ch \#) [0 y x])) xs))
                (range))
        set))
 
-(defn neighbors [[z y x]]
+(defn neighbors-3d [[z y x]]
   (for [dz [-1 0 1]
         dy [-1 0 1]
         dx [-1 0 1]
         :when (not= [0 0 0] [dz dy dx])]
     [(+ z dz) (+ y dy) (+ x dx)]))
 
-(defn step [state]
+(defn step [neighbors state]
   (set (for [[pos n] (frequencies (mapcat neighbors state))
              :when   (or (and (state pos)
                             (#{2 3} n))
@@ -28,17 +28,40 @@
          pos)))
 
 (->> test-input
-     index
-     (iterate step)
+     index-3d
+     (iterate (partial step neighbors-3d))
      (drop 6)
      first
      count)
 
 ;; part 1
 (->> (slurp "inputs/day-17.txt")
-     index
-     (iterate step)
+     index-3d
+     (iterate (partial step neighbors-3d))
      (drop 6)
      first
      count)
 ;; => 207
+
+(defn index-4d [input]
+  (->> (str/split-lines input)
+       (mapcat (fn [y xs]
+                 (keep-indexed (fn [x ch] (when (= ch \#) [0 0 y x])) xs))
+               (range))
+       set))
+
+(defn neighbors-4d [[w z y x]]
+  (for [dw [-1 0 1]
+        dz [-1 0 1]
+        dy [-1 0 1]
+        dx [-1 0 1]
+        :when (not= [0 0 0 0] [dw dz dy dx])]
+    [(+ w dw) (+ z dz) (+ y dy) (+ x dx)]))
+
+(->> (slurp "inputs/day-17.txt")
+     index-4d
+     (iterate (partial step neighbors-4d))
+     (drop 6)
+     first
+     count)
+;; => 2308
