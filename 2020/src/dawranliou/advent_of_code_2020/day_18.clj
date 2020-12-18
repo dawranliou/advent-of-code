b8a6f593e663a5504ae2baec8421ab71c37074c2
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-12-18 19:08:29 +0800

    Improve day 18 part 1

--- 2020/src/dawranliou/advent_of_code_2020/day_18.clj
+++ 2020/src/dawranliou/advent_of_code_2020/day_18.clj
@@ -1,19 +1,28 @@
 (ns dawranliou.advent-of-code-2020.day-18
-  (:require [dawranliou.aoc :as aoc]
+  (:require [clojure.edn :as edn]
             [clojure.walk :as w]
-            [clojure.edn :as edn]))
+            [dawranliou.aoc :as aoc]))
 
 (defn parse [s]
   (edn/read-string (str "(" s ")")))
 
 (defn eval-math [l]
-  (let [pre  (first l)
-        tail (rest l)]
-    (reduce (fn [total [op n]]
-              (({'+ + '* *} op) total (if (list? n) (eval-math n) n)))
-            (if (list? pre) (eval-math pre) pre)
-            (partition 2 tail))))
+  (w/postwalk
+    (fn [form]
+      (if (sequential? form)
+        (->> (partition 2 (rest form))
+             (reduce
+               (fn [total [op n]]
+                 ((resolve op) total n))
+               (first form)))
+        form))
+    l))
+
+(eval-math '(1 + 2 * 3 + 4 * 5 + 6))
+;; => 71
+(eval-math '(1 + (2 * 3) + (4 * (5 + 6))))
+;; => 51
 
 ;; part 1
-(->> (aoc/with-line "day-18.txt" (comp eval-math parse) #(apply + %)))
+(aoc/with-line "day-18.txt" (comp eval-math parse) #(apply + %))
 ;; => 202553439706
