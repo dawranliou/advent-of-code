7ebcc290117095c47bc241dc084eaa9f38fd8d20
Daw-Ran Liou <dawran.liou@kirasystems.com>
2020-12-21 15:05:24 +0800

    Day 19 part 2

--- 2020/src/dawranliou/advent_of_code_2020/day_19.clj
+++ 2020/src/dawranliou/advent_of_code_2020/day_19.clj
@@ -5,17 +5,33 @@
 
 (def input (slurp "inputs/day-19.txt"))
 
-(def rules
+(def sorted-input
   (->> (str/split input #"\n\n")
        first
        str/split-lines
        (sort-by #(Integer/parseInt (second (re-find #"(\d+): " %))))
-       (str/join "\n")
-       insta/parser))
+       (str/join "\n")))
 
-(->> (str/split input #"\n\n")
-     second
-     str/split-lines
+(def rules
+  (insta/parser sorted-input))
+
+(def examples
+  (->> (str/split input #"\n\n")
+       second
+       str/split-lines))
+
+(->> examples
      (map rules)
+     (remove insta/failure?)
+     count)
+
+(def rules-2
+  (-> sorted-input
+      (str/replace #"\n8\: 42\n" "\n8: 42 | 42 8\n")
+      (str/replace #"\n11\: 42 31\n" "\n11: 42 31 | 42 11 31\n")
+      insta/parser))
+
+(->> examples
+     (map rules-2)
      (remove insta/failure?)
      count)
